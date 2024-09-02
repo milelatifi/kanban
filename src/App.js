3817ca7f07302f97a3cb644f63d192b8c0b4e0b7
Mile Latifi <milelatifi2002@gmail.com>
2024-09-02 21:48:37 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const card = (title, tasks, id) => { 
+  return <div className="bg-white shadow-lg rounded-lg overflow-hidden mb-4 max-w-md w-full">
+  <div className="bg-red-500 p-4 text-xl font-semibold text-white">
+      {title}
+  </div>
+    
+    <div className="flex gap-4 justify-between py-2 px-4 items-center text-gray-700">
+      <span>{id}</span>
+      <span>{tasks}</span>
+    </div>
+</div>
+ }
 
 function App() {
+  const title = 'To Do';
+  const tasks = 'Work 8h';
+  const id = 'Monday';
+
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+      {card (title, tasks, id)}
+      {card ('In process', 'Adjust the shadow of the info box', 'Monday')}
+      {card ('Done', ' Change the backgroud color', 'Monday')}
+      {card ('In Process', 'Remove the opacity of the images', 'Monday')}
+      {card ('Done', ' Add Responsivity to the page', 'Monday')}
+      {card ('To do', 'Check if font-size is 22px', 'Monday')}
+      {card ('To Do', 'Add a flower icon ', 'Monday')}
+      {card ('Done', ' Update profile picture ', 'Monday')}
+  
+    </>
   );
 }
 
